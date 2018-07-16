031f663adc16c832637412f2222ec6c07cac18a2
dorianinc <dorianrdba@gmail.com>
2018-07-16 03:20:01 -0700

    Get data to appear on results page

--- padlily/models/index.js
+++ padlily/models/index.js
@@ -1,8 +1,3 @@
-// Exporting an object containing all of our models
-
 module.exports = {
-  Renter: require("./Renter"),
-  Application: require("./Application"),
-  PropertyManager: require("./PropertyManager"),
   Property: require("./Property")
 };
--- padlily/routes/api/properties.js
+++ padlily/routes/api/properties.js
@@ -1,13 +1,16 @@
 const router = require("express").Router();
 const propertiesController = require("../../controllers/propertiesController");
 
-// Matches with "/api/books"
-router.route("/results")
+// Matches with "/api/properties"
+router.route("/")
   .get(propertiesController.findAll)
+  .post(propertiesController.create);
 
-// Matches with "/api/books/:id"
+// Matches with "/api/properties/:id"
 router
   .route("/:id")
   .get(propertiesController.findById)
+  .put(propertiesController.update)
+  .delete(propertiesController.remove);
 
 module.exports = router;
