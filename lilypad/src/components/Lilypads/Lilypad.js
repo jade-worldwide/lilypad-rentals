2834f508f34feb7d727de81ed9634ec4c13370f9
Julian Hamilton <Ham@airbears2-10-142-51-128.airbears2.1918.berkeley.edu>
2018-07-21 14:37:19 -0700

    working disp component

--- lilypad/src/components/Lilypads/Lilypad.js
+++ lilypad/src/components/Lilypads/Lilypad.js
@@ -1,7 +1,13 @@
 import React, { Component } from 'react';
 import lilypad from '../../favicon.ico';
+import 'bulma/css/bulma.css';
+import "./Lilypad.css";
 
-
+const Dispbox = (props) => (
+    <div class="pad">{props.text}
+    <img width="15px" src={props.image}/>
+    </div>
+);
 
 class Toog extends Component {
 
@@ -14,10 +20,12 @@ class Toog extends Component {
 
 
       render() {
+          console.log(this.state)
+          console.log(this.props)
           return (
             <div>
                 <img onClick={this.handleClick} width="10px" src={lilypad} /> 
-                {this.state.isBoxVisible ? 'visib' : 'invisib'}               
+                {this.state.isBoxVisible ? <Dispbox text={this.props.text} image={this.props.image} /> : ''}               
             </div>
           )
       }
