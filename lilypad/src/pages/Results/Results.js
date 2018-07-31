d16d936f410ec16e672d6cf8fca7b760c3038e6f
andrewhemans <andrewhemans@gmail.com>
2018-07-30 22:03:22 -0700

    results page mostly finished

--- lilypad/src/pages/Results/Results.js
+++ lilypad/src/pages/Results/Results.js
@@ -1,21 +1,45 @@
 import React, { Component } from "react";
 import { ResultsList, Filters } from "../../components/ResultsList";
+import { Button } from 'bloomer';
+import { ResultsMap } from "../../components/ResultsMap";
+import { GoogleMap } from "../../components/GoogleMap";
 import "./Results.css";
 
 
 class Results extends Component {
-  // Setting our component's initial state
-  state = {
-  };
 
 
+// Setting our component's initial state
+state = {
+  show: "columns results-columns",
+};
+
+  filtersShow = () => {
+    this.setState({ show: "columns results-columns filters-margin" })
+  }
+
+  filtersHide = () => {
+    this.setState({ show: "columns results-columns" })
+  }
 
 
   render() {
     return (
       <div className="results">
-        <Filters />
-        <ResultsList />
+        <Filters handler = {this.handler} />
+        <div className={this.state.show}>
+          <div className="column results-column list-column">
+            <div className="filter-header">
+              <Button isColor='primary' className="show-filters" onClick={this.filtersShow}><p>Filters</p></Button>
+            </div>
+            <div className="result-list">
+              <ResultsList />
+            </div>
+          </div>
+          <div className="column results-column">
+            <GoogleMap />
+          </div>
+        </div>
       </div>
     );
   }
