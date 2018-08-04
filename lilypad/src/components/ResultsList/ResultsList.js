286ddd8b212363191652d67496fc02f5c112cf03
andrewhemans <andrewhemans@gmail.com>
2018-08-04 12:15:30 -0700

    liked buttons toggle now

--- lilypad/src/components/ResultsList/ResultsList.js
+++ lilypad/src/components/ResultsList/ResultsList.js
@@ -1,30 +1,46 @@
-import React from "react";
+import React, { Component } from "react";
 import { Table, Image, Subtitle, Button } from 'bloomer';
 import 'bulma/css/bulma.css';
 import "./ResultsList.css";
 
-export const ResultsList = () => (
-  <div className="results-list">
+export class ResultsList extends Component {
+
+  state = {
+    liked: "far fa-heart",
+  };
+
+    showLiked = () => {
+      this.setState({ liked: "fas fa-heart is-liked" })
+    }
+
+    unlike = () => {
+      this.setState({ liked: "far fa-heart" })
+    }
+
+    render() {
+        return (
+
+          <div className="results-list">
 
 
 
-    <Table>
-      <tbody>
-          <tr className="results-row">
-              <td className="list-image"><Image isSize="96x96" src="https://via.placeholder.com/96x96" /></td>
-              <td className="results-info">
-              <p className="results-title">Sunny lakeside cottage with beautiful garden.</p>
-              <p className="results-subtitle">$2000 - 2 Bedrooms - 925sqft</p>
-              </td>
-              <td className="list-like">
-                <Button isColor='white' className="like-button" isLiked={false}><p><i className="far fa-heart"></i></p></Button>
-              </td>
-          </tr>
-      </tbody>
-    </Table>
+            <Table>
+              <tbody>
+                  <tr className="results-row">
+                      <td className="list-image"><Image isSize="96x96" src="https://via.placeholder.com/96x96" /></td>
+                      <td className="results-info">
+                      <p className="results-title">Sunny lakeside cottage with beautiful garden.</p>
+                      <p className="results-subtitle">$2000 - 2 Bedrooms - 925sqft</p>
+                      </td>
+                      <td className="list-like">
+                        <Button isColor='white' className="like-button" onClick={this.state.liked === "far fa-heart" ? this.showLiked : this.unlike}><p><i className={this.state.liked}></i></p></Button>
+                      </td>
+                  </tr>
+              </tbody>
+            </Table>
 
+          </div>
 
-  </div>
-
-
-);
+        );
+      }
+}
